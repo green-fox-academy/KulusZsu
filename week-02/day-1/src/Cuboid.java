4801590c814df993e71be7f67152dc27eb4f3fae
KulusZsu <kulus.zsuzsanna@kodo.hu>
2018-10-29 16:20:04 +0100

    add Cuboid exercise

---
+++ week-02/day-1/src/Cuboid.java
@@ -0,0 +1,16 @@
+public class Cuboid {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 5;
+        int c = 5;
+
+        int surf;
+        surf = 6 * (a * a);
+        int vol;
+        vol = (a * b * c);
+
+        System.out.println("Surface Area: " + surf);
+        System.out.println("Volume: " + vol);
+
+    }
+}
