8e0ea10a0292be5ce50466c3447111e2cf2bdabc
KulusZsu <kulus.zsuzsanna@kodo.hu>
2018-10-29 12:37:53 +0100

    add HelloOthers exercise

---
+++ week-02/day-1/src/HelloOthers.java
@@ -0,0 +1,13 @@
+public class HelloOthers {
+    public static void main(String[] args) {
+        // Greet 3 of your classmates with this program, in three separate lines
+        // like:
+        //
+        // Hello, Esther!
+        // Hello, Mary!
+        // Hello, Joe!
+        System.out.println("Hello, Csirke!");
+        System.out.println("Hello, Dodo!");
+        System.out.println("Hello, Kristof!");
+    }
+}
