b84fadce296d469e66cdba6652f2441bbcbf4c39
KulusZsu <kulus.zsuzsanna@kodo.hu>
2018-10-29 17:40:46 +0100

    add OddEven exercise

---
+++ week-02/day-1/src/OddEven.java
@@ -0,0 +1,14 @@
+// Write a program that reads a number from the standard input,
+// Then prints "Odd" if the number is odd, or "Even" if it is even.
+public class OddEven {
+    public static void main(String[] args) {
+
+        int a = 5;
+        if (a % 2 == 0) {System.out.println("Odd");}
+        if (a % 2 == 1) {System.out.println("Even");}
+
+    }
+}
+
+
+
