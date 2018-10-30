7652b531ced304840d7d8f4dfd552afd7b52e39f
KulusZsu <kulus.zsuzsanna@kodo.hu>
2018-10-30 12:10:54 +0100

    add HelloWord example-exercise

---
+++ week-02/day-2/src/HelloWord.java
@@ -0,0 +1,11 @@
+public class HelloWord {
+
+    public static void main(String[] args) {
+        greetFunction();
+    }
+
+    public static void greetFunction() {
+        //Prints "Hello Green Fox" to the terminal window.
+        System.out.println("Hello Green Fox");
+    }
+}
