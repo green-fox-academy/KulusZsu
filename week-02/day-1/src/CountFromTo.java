af1a0f1a8ad59d5989ee59e2ef6d8bde8570aefa
KulusZsu <kulus.zsuzsanna@kodo.hu>
2018-10-31 13:00:13 +0100

    add CountFromTo exercise

---
+++ week-02/day-1/src/CountFromTo.java
@@ -0,0 +1,40 @@
+// Create a program that asks for two numbers
+// If the second number is not bigger than the first one it should print:
+// "The second number should be bigger"
+//
+// If it is bigger it should count from the first number to the second by one
+//
+// example:
+//
+// first number: 3, second number: 6, should print:
+//
+// 3
+// 4
+// 5
+
+import java.util.Scanner;
+public class CountFromTo {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int number1;
+        int number2;
+
+        System.out.println("Please give me two numbers!");
+
+        number1 = scanner.nextInt();
+        number2 = scanner.nextInt();
+
+        if (number1 >= number2) {
+            System.out.println("The second number should be bigger");
+        }
+        if (number1 < number2) {
+            for (int i = number1; i < number2; i++) {
+            System.out.println(i);
+        }
+
+        }
+    }
+}
