2b66236c26def56aaff5017910e4bf73dba0df3b
KulusZsu <kulus.zsuzsanna@kodo.hu>
2018-11-07 17:06:10 +0100

    add PurpleSteps exercise

---
+++ week-03/day-3/src/PurpleSteps.java
@@ -0,0 +1,48 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class PurpleSteps {
+    public static void mainDraw(Graphics graphics) {
+
+        //based the picture on Github
+
+        for (int i = 0; i < 200; i += 15) {
+            int a = i;
+            drawRect(graphics, a);
+            }
+
+    }
+    public static void drawRect(Graphics graphics, int a) {
+        graphics.setColor(new Color(153, 50, 204));
+        graphics.fillRect(15 + a, 15 + a, 15, 15);
+        graphics.setColor(Color.BLACK);
+        graphics.drawRect(15 + a, 15 + a, 15, 15);
+
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
