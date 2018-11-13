caeae1451c2622be706bf369f94b30ebba004e91
KulusZsu <kulus.zsuzsanna@kodo.hu>
2018-11-13 13:01:48 +0100

    add Animal exercise

--- week-04/day-2/Animal/src/exercise/company/Animal.java
+++ week-04/day-2/Animal/src/exercise/company/Animal.java
@@ -1,4 +1,25 @@
 package exercise.company;
 
 public class Animal {
+  int hunger;
+  int thirst;
+
+  public Animal(int hunger, int thirst) {
+    this.hunger = 50;
+    this.thirst = 50;
+  }
+
+  public int eat() {
+    return hunger -=1;
+  }
+
+  public int drink() {
+    return thirst -=1;
+
+  }
+
+  public int play() {
+    return hunger +=1;
+    return thirst +=1;
+  }
 }
---
+++ week-04/day-2/Animal/src/exercise/company/Main.java
@@ -0,0 +1,11 @@
+package exercise.company;
+
+public class Main {
+
+  public static void main(String[] args) {
+    // write your code here
+    Animal doggo = new Animal();
+    Animal kitty = new Animal();
+
+  }
+}
