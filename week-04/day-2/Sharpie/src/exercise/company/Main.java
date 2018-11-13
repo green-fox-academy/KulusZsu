9178d672a1cc12b2fc9ddacba095489af87516f2
KulusZsu <kulus.zsuzsanna@kodo.hu>
2018-11-13 14:47:21 +0100

    add Sharpie exercise

---
+++ week-04/day-2/Sharpie/src/exercise/company/Main.java
@@ -0,0 +1,12 @@
+package exercise.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Sharpie firstOne = new Sharpie("yello", 7);
+        firstOne.use();
+
+        System.out.println(firstOne.inkAmount);
+    }
+}
