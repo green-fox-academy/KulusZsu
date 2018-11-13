087d68e800950b9503901f51bacb8d115990dae3
KulusZsu <kulus.zsuzsanna@kodo.hu>
2018-11-13 16:59:21 +0100

    add Pokemon exercise

---
+++ week-04/day-2/Pokemon/src/exercise/company/Main.java
@@ -0,0 +1,43 @@
+package exercise.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+  public static void main(String[] args) {
+    List<Pokemon> pokemonOfAsh = initializePokemons();
+
+    // Every pokemon has a name and a type.
+    // Certain types are effective against others, e.g. water is effective against fire.
+
+    // Ash has a few pokemon.
+    // A wild pokemon appeared!
+
+    Pokemon wildPokemon = new Pokemon("Oddish", "leaf", "water");
+
+    // Which pokemon should Ash use?
+
+    // THE POKEMON.JAVA BOOLEAN WILL EXAMINE THE POKEMONS' ARRAY LIST, IF THERE IS EQUALITY IN TYPE,
+    // THE BOOLEAN WILL TRUE!!! --> THAT IS WHY WE HAVE TO EXAMINE WITH AN FOR/IF LOOP THE ELEMENTS IN
+    // THE ARRAY LIST WITH pokemonOfAsh.
+
+    for (int i = 0; i < pokemonOfAsh.size(); i++) {
+      if (pokemonOfAsh.get(i).isEffectiveAgainst(wildPokemon)) {
+        System.out.print("I choose you, " + pokemonOfAsh.get(i).name);
+      }
+    }
+  }
+
+  private static List<Pokemon> initializePokemons() {
+    List<Pokemon> pokemon = new ArrayList<>();
+
+    pokemon.add(new Pokemon("Balbasaur", "leaf", "water"));
+    pokemon.add(new Pokemon("Pikatchu", "electric", "water"));
+    pokemon.add(new Pokemon("Charizard", "fire", "leaf"));
+    pokemon.add(new Pokemon("Balbasaur", "water", "fire"));
+    pokemon.add(new Pokemon("Kingler", "water", "fire"));
+
+    return pokemon;
+  }
+}
+
