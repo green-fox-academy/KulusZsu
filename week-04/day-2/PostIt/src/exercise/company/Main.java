b08033f31fa4e42ce7cc2709635bf4be55f1487a
KulusZsu <kulus.zsuzsanna@kodo.hu>
2018-11-13 12:07:44 +0100

    add PostIt exercise

---
+++ week-04/day-2/PostIt/src/exercise/company/Main.java
@@ -0,0 +1,11 @@
+package exercise.company;
+
+public class Main {
+
+  public static void main(String[] args) {
+    // write your code here
+        PostIt first = new PostIt("orange", "Idea 1", "blue");
+        PostIt second = new PostIt("pink", "Awesome", "black");
+        PostIt third = new PostIt("yellow","Superb!","green");
+  }
+}
