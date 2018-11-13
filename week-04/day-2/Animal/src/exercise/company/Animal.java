2d9090486e8ce953b72264bc5e59955ffad80781
KulusZsu <kulus.zsuzsanna@kodo.hu>
2018-11-13 14:27:18 +0100

    add Animal exercise

--- week-04/day-2/Animal/src/exercise/company/Animal.java
+++ week-04/day-2/Animal/src/exercise/company/Animal.java
@@ -4,22 +4,21 @@ public class Animal {
   int hunger;
   int thirst;
 
-  public Animal(int hunger, int thirst) {
+  public Animal() {
     this.hunger = 50;
     this.thirst = 50;
   }
 
   public int eat() {
-    return hunger -=1;
+    return hunger -= 1;
   }
 
   public int drink() {
-    return thirst -=1;
-
+    return thirst -= 1;
   }
 
-  public int play() {
-    return hunger +=1;
-    return thirst +=1;
+  public void play() {
+    hunger += 1;
+    thirst += 1;
   }
 }
--- week-04/day-2/Animal/src/exercise/company/Main.java
+++ week-04/day-2/Animal/src/exercise/company/Main.java
@@ -5,7 +5,12 @@ public class Main {
   public static void main(String[] args) {
     // write your code here
     Animal doggo = new Animal();
-    Animal kitty = new Animal();
+    System.out.println(doggo.drink());
+    System.out.println(doggo.eat());
 
+    Animal kitty = new Animal();
+    System.out.println(kitty.drink());
+    System.out.println(kitty.eat());
   }
 }
+
