895b71672f27fffc96f6b5b2a753fb7d84db5dcc
KulusZsu <kulus.zsuzsanna@kodo.hu>
2018-11-14 14:46:10 +0100

    do again PostIt

---
+++ repeated_tasks/week-4-file-object-class/object,class/PostIt/src/exercise/company/Main.java
@@ -0,0 +1,11 @@
+package exercise.company;
+
+public class Main {
+
+  public static void main(String[] args) {
+    // write your code here
+    PostIt one = new PostIt("orange", "Idea 1", "blue");
+    PostIt two = new PostIt("pink", "Awesome", "back");
+    PostIt three = new PostIt("yello", "Superb!", "green");
+  }
+}
