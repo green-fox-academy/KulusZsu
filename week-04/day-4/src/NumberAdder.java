083a7d2ad2843a151df13aaef944e9369102cd75
KulusZsu <kulus.zsuzsanna@kodo.hu>
2018-11-15 11:25:49 +0100

    add Number Adder exercise

---
+++ week-04/day-4/src/NumberAdder.java
@@ -0,0 +1,17 @@
+// Write a recursive function that takes one parameter: n and adds numbers from 1 to n.
+
+public class NumberAdder {
+
+  public static void main(String[] args) {
+    System.out.println(reCursive(5));
+  }
+
+  public static int reCursive(int number) {
+    if (number < 0) {
+      return 0;
+    } else {
+      return reCursive(number - 1) + number;
+    }
+  }
+}
+
